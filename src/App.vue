819cad84698a7f0c469664fa2006c798655db7cc
Alexey Desyatnik <alexey.desyatnik@gmail.com>
2021-12-07 13:27:49 +0300

    Added custom rendering for title and body table cells

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div id="app">
-    <h1>Hi there</h1>
     <my-table :rows="rows">
       <my-table-column prop="id" title="ID" />
       <my-table-column prop="postId" title="Post ID" />
-      <my-table-column prop="email" title="Email" />
+      <my-table-column prop="email">
+        <template #title>
+          <i>Email</i>
+        </template>
+        <template #body="{ row }">
+          <a :href="`mailto:${row.email}`">{{ row.email }}</a>
+        </template>
+      </my-table-column>
       <my-table-column prop="name" title="Name" />
     </my-table>
   </div>
--- src/components/MyTable.vue
+++ src/components/MyTable.vue
@@ -15,16 +15,31 @@ export default {
           item.componentOptions.tag === 'my-table-column'
       );
       const options = columns.map((column) =>
-        Object.assign({}, column.componentOptions.propsData)
+        Object.assign({}, column.componentOptions.propsData, {
+          scopedSlots: column.data.scopedSlots || {},
+        })
       );
       return options;
     },
     renderHead(h, columnOptions) {
-      const head = columnOptions.map((column) => <th>{column.title}</th>);
+      const head = columnOptions.map((column) => {
+        const title = column.scopedSlots.title
+          ? column.scopedSlots.title()
+          : column.title;
+        const headCell = <th key={column.prop}>{title}</th>;
+        return headCell;
+      });
       return head;
     },
     renderCells(h, row, columnOptions) {
-      const cells = columnOptions.map((column) => <td>{row[column.prop]}</td>);
+      const cells = columnOptions.map((column) => {
+        const data = row[column.prop];
+        const body = column.scopedSlots.body
+          ? column.scopedSlots.body({ row })
+          : data;
+        const bodyCell = <td key={column.prop}>{body}</td>;
+        return bodyCell;
+      });
       return cells;
     },
     renderRows(h, columnOptions) {
@@ -40,7 +55,6 @@ export default {
   },
   render(h) {
     const columnOptions = this.getColumnOptions();
-    console.log(columnOptions);
     const head = this.renderHead(h, columnOptions);
     const rows = this.renderRows(h, columnOptions);
     return (
