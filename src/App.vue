3db1a05c2c6e1bd352ba17a2daef83e720b96b10
Alexey Desyatnik <alexey.desyatnik@gmail.com>
2021-12-07 10:56:49 +0300

    Added bare-bones table rendering

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <h1>Hi there</h1>
-    <my-table></my-table>
+    <my-table :rows="rows"></my-table>
   </div>
 </template>
 
--- src/components/MyTable.vue
+++ src/components/MyTable.vue
@@ -7,11 +7,27 @@ export default {
       default: () => [],
     },
   },
-  render(/* h */) {
+  methods: {
+    renderCells(h, row) {
+      const cells = Object.entries(row).map(([, value]) => {
+        return <td>{value}</td>;
+      });
+      return cells;
+    },
+    renderRows(h) {
+      const rows = this.rows.map((row, index) => {
+        console.log(row);
+        return <tr key={row.id || index}>{...this.renderCells(h, row)}</tr>;
+      });
+      return rows;
+    },
+  },
+  render(h) {
+    const rows = this.renderRows(h);
     return (
       <table>
         <thead></thead>
-        <tbody></tbody>
+        <tbody>{...rows}</tbody>
       </table>
     );
   },
